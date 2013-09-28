40a55ab3ecc56308ba8c2329a24243f62c2529d4
Myron Tuttle <myrongt@gmail.com>
2013-09-28 17:27:43 -0600

    Renamed mount point to trade/adapt. Removed static files.

--- com.myrontuttle.fin.trade.web/src/com/myrontuttle/fin/trade/web/BasicPage.java
+++ com.myrontuttle.fin.trade.web/src/com/myrontuttle/fin/trade/web/BasicPage.java
@@ -6,7 +6,9 @@ import org.apache.wicket.markup.html.basic.Label;
 public class BasicPage extends WebPage {
 
 	public BasicPage() {
-		add(new Label("message", "Hello World!"));
+		add(new Header("mainNavigation", "Adaptive Trader", this));
+		
+		add(new Label("message", "Let's make a trade!"));
 	}
 
 }

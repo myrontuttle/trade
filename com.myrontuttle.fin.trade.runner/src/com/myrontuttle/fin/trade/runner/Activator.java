f5af324abde47b2459d1a17c0d9a8fc6b5ede3f9
Myron Tuttle <myrongt@gmail.com>
2013-10-17 21:11:21 -0600

    Initial Trade Runner commit

---
+++ com.myrontuttle.fin.trade.runner/src/com/myrontuttle/fin/trade/runner/Activator.java
@@ -0,0 +1,24 @@
+package com.myrontuttle.fin.trade.runner;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Starting Trade Runner");
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Stopping Trade Runner");
+	}
+
+}

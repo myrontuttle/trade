23eeabcb798e945902d43185948363919da10c1e
Myron Tuttle <myrongt@gmail.com>
2013-10-31 19:41:11 -0600

    Using blueprint service reference to operate StrategyDAO

--- com.myrontuttle.fin.trade.runner/src/com/myrontuttle/fin/trade/runner/Activator.java
+++ com.myrontuttle.fin.trade.runner/src/com/myrontuttle/fin/trade/runner/Activator.java
@@ -2,77 +2,80 @@ package com.myrontuttle.fin.trade.runner;
 
 import java.util.List;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 import com.myrontuttle.fin.trade.adapt.Group;
 import com.myrontuttle.fin.trade.adapt.StrategyDAO;
 
-public class Activator implements BundleActivator {
+public class Activator {
 
     private StrategyDAO strategyDAO;
     
     public void setStrategyDAO(StrategyDAO sdao) {
+    	System.out.println("Setting StrategyDAO");
     	this.strategyDAO = sdao;
     }
-    
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		System.out.println("Starting Trade Runner");
-		if (strategyDAO == null) {
+
+    public void bind(ServiceReference<?> reference) {
+        System.out.println("Activator, service bound: " + reference);
+        
+        if (strategyDAO == null) {
 			System.out.println("No StrategyDAO");
 			return;
-		}
-		List<Group> existingGroups = strategyDAO.findGroups();
-		if (existingGroups != null && existingGroups.size() > 0) {
-			System.out.println("Groups (id - start date): ");
-			for (Group group : existingGroups) {
-				System.out.println(group.getGroupId() + " - " + group.getStartTime().toString());
-			}
 		} else {
-			System.out.println("No existing groups. Creating new group");
-			Group group = strategyDAO.newGroupRecord();
-			group.setAlertAddress("wsodinvestor@gmail.com");
-			group.setActive(true);
-			group.setAlertsPerSymbol(2);
-			group.setEliteCount(3);
-			group.setSize(15);
-			group.setEvaluationStrategy(Group.RANDOM_EVALUATOR);
-			group.setExpressionStrategy(Group.NO_EXPRESSION);
-			group.setFrequency(Group.DAILY);
-			group.setNumberOfScreens(1);
-			group.setMaxSymbolsPerScreen(5);
-			group.setMutationFactor(.05);
-			strategyDAO.saveGroup(group);
-			System.out.println("Group " + group.getGroupId() + ", created on: " + 
-								group.getStartTime().toString());
+			System.out.println("Found StrategyDAO");
 		}
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		System.out.println("Stopping Trade Runner");
-
-		if (strategyDAO == null) {
-			System.out.println("No StrategyDAO");
+		
+		try {
+			System.out.println("Finding Simple Groups");
+			List<Group> existingGroups = strategyDAO.findGroups();
+			if (existingGroups != null && existingGroups.size() > 0) {
+				System.out.println("Groups (id): ");
+				for (Group group : existingGroups) {
+					System.out.println(group.getGroupId());
+				}
+			} else {
+				System.out.println("No existing groups. Creating new group");
+				Group group = strategyDAO.newGroupRecord();
+				/*
+				group.setAlertAddress("wsodinvestor@gmail.com");
+				group.setActive(true);
+				group.setAlertsPerSymbol(2);
+				group.setEliteCount(3);
+				group.setSize(15);
+				group.setEvaluationStrategy(Group.RANDOM_EVALUATOR);
+				group.setExpressionStrategy(Group.NO_EXPRESSION);
+				group.setFrequency(Group.DAILY);
+				group.setNumberOfScreens(1);
+				group.setMaxSymbolsPerScreen(5);
+				group.setMutationFactor(.05);
+				*/
+				strategyDAO.saveGroup(group);
+				System.out.println("Group " + group.getGroupId() + ", created on: " + 
+									group.getStartTime().toString());
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 			return;
 		}
+    }
 
-		List<Group> existingGroups = strategyDAO.findGroups();
-		if (existingGroups != null && existingGroups.size() > 0) {
-			System.out.println("Groups (id - start date): ");
-			for (Group group : existingGroups) {
-				System.out.println(group.getGroupId() + " - " + group.getStartTime().toString());
-			}
-		} else {
-			System.out.println("No existing groups.");
-		}
-	}
+    public void unbind(ServiceReference<?> reference) {
+        System.out.println("Activator, service unbound: " + reference);
+
+        try {
+    		List<Group> existingGroups = strategyDAO.findGroups();
+    		if (existingGroups != null && existingGroups.size() > 0) {
+    			System.out.println("Groups (id - start date): ");
+    			for (Group group : existingGroups) {
+    				System.out.println(group.getGroupId() + " - " + group.getStartTime().toString());
+    			}
+    		} else {
+    			System.out.println("No existing groups.");
+    		}
+        } catch (Exception e) {
+        	System.out.println(e.getMessage());
+        }
+    }
 
 }
